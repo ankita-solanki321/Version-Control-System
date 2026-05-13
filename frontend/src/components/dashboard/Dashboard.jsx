c90d47f82fc8f8168adb07c31a2bb8ff1ffe04ac
Ankita Solanki <ankita@gmail.com>
2026-05-13 22:19:36 +0530

    building user profile

--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
-
+import "./dashboard.css";
+import Navbar from "../Navbar";
 const Dashboard = () => {
   const [repositories, setRepositories] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [suggestedRepositories, setSuggestedRepositories] = useState([]);
   const [searchResults, setSearchResults] = useState([]);
 
+
+  //user specific repo
 useEffect(() => {
     const userId = localStorage.getItem("userId");
 
@@ -23,6 +26,7 @@ useEffect(() => {
       }
     };
 
+    // all repos in db
     const fetchSuggestedRepositories = async () => {
       try {
         const response = await fetch(`http://localhost:3000/repo/all`);
@@ -37,5 +41,72 @@ useEffect(() => {
     fetchRepositories();
     fetchSuggestedRepositories();
   }, []);
+
+
+
+  // user can serach there specific repos
+   useEffect(() => {
+    if (searchQuery == "") {
+      setSearchResults(repositories);
+    } else {
+      const filteredRepo = repositories.filter((repo) =>
+        repo.name.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+      setSearchResults(filteredRepo);
+    }
+  }, [searchQuery, repositories]);
+  
+   return (
+    <>
+       <Navbar /> 
+      <section id="dashboard">
+        <aside>
+          <h3>Suggested Repositories</h3>
+          {suggestedRepositories.map((repo) => {
+            return (
+              <div key={repo._id}>
+                <h4>{repo.name}</h4>
+                <h4>{repo.description}</h4>
+              </div>
+            );
+          })}
+        </aside>
+        <main>
+          <h2>Your Repositories</h2>
+          <div id="search">
+            <input
+              type="text"
+              value={searchQuery}
+              placeholder="Search..."
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+          </div>
+          {searchResults.map((repo) => {
+            return (
+              <div key={repo._id}>
+                <h4>{repo.name}</h4>
+                <h4>{repo.description}</h4>
+              </div>
+            );
+          })}
+        </main>
+        <aside>
+          <h3>Upcoming Events</h3>
+          <ul>
+            <li>
+              <p>Tech Conference - Dec 15</p>
+            </li>
+            <li>
+              <p>Developer Meetup - Dec 25</p>
+            </li>
+            <li>
+              <p>React Summit - Jan 5</p>
+            </li>
+          </ul>
+        </aside>
+      </section>
+    </>
+  );
+
 }
 export default Dashboard;
